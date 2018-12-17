a531d6e693a6929ac74725c6d9324659cbfde45a
Ritesh Singh <1124ritesh@gmail.com>
2018-12-17 17:22:20 +0530

    users schema
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

---
+++ backend/models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = users = mongoose.model("users", UserSchema);
