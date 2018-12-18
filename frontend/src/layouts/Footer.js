75091e7b93897266ab89b325863364ee90f89d48
Ritesh Singh <1124ritesh@gmail.com>
2018-12-18 20:26:32 +0530

    layout
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

---
+++ frontend/src/layouts/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="bg-light text-dark mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} MernStarter
+    </footer>
+  );
+};
