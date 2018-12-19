3cb8f21d766cc3fd34f75e39918a31494c6e0632
Ritesh Singh <1124ritesh@gmail.com>
2018-12-19 17:49:56 +0530

    redux setup
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./App.css";
 import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
@@ -9,15 +11,17 @@ import Login from "./components/auth/Login";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Header />
-          <Route exact path="/" component={Welcome} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
